5525c5ce20354f210d04560359bd5ab9e8aa585a
dylancodeverse <ratianarivodylan@gmail.com>
2024-03-26 18:22:36 +0300

    env preparation

--- JSP-NO-CODE/outputTemp/PromotionRepository.java
+++
@@ -1,9 +0,0 @@
-package scaffold.framework.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository ;
-
-import scaffold.framework.demo.entity.PromotionModel ;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
-}
-
